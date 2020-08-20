f60d8d1043e6a607d850b8c365d0afd43d98631f
Benjamin Meena <59247187+BMeeNA57@users.noreply.github.com>
2020-08-20 11:36:34 -0600

    Updates

---
+++ precache-manifest.610c246f7b60c69c710f0e58e2a37193.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b47da7fe43cfd329841c91c5af84e724",
+    "url": "/reactpokemon/index.html"
+  },
+  {
+    "revision": "e8f692583c861d74f57c",
+    "url": "/reactpokemon/static/js/2.e62a55d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactpokemon/static/js/2.e62a55d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42c76c7bb3b30b0ae819",
+    "url": "/reactpokemon/static/js/main.b8f2817f.chunk.js"
+  },
+  {
+    "revision": "78b45750e802b2145049",
+    "url": "/reactpokemon/static/js/runtime-main.a4386558.js"
+  }
+]);
